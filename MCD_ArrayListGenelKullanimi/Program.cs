058ce8ca24fbe73a4d32fff2ddcbd4cb459b63e1
Faik Turan <turanfaik@hotmail.com>
2021-11-22 16:05:00 +0300

    ArrayList anlatıldı

--- MCD_ArrayListGenelKullanimi/Program.cs
+++ MCD_ArrayListGenelKullanimi/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace MCD_ArrayListGenelKullanimi
 {
@@ -10,6 +11,32 @@ namespace MCD_ArrayListGenelKullanimi
     {
         static void Main(string[] args)
         {
+            #region ArrayList Genel Kullanimi
+            //10 elemanlı (strin) ArrayList içindeki değerleri Z-A olacak şekilde sıralayın.
+            ArrayList odevListe = new ArrayList();
+            odevListe.Add("Fatih");
+            odevListe.Add("Esra");
+            odevListe.Add("Ali Mert");
+            odevListe.Add("Serdar");
+            odevListe.Add("Gamze");
+            odevListe.Add("Hakan");
+            odevListe.Add("Furkan");
+            odevListe.Add("Birol");
+            odevListe.Add("Cem");
+            odevListe.Add("Yakup");
+
+            //Tüm değerleri A-z çevir
+            odevListe.Sort();
+            odevListe.Reverse();
+
+            foreach (var item in odevListe)
+            {
+                Console.WriteLine(item);
+            }
+
+            
+            Console.ReadLine();
+            #endregion
         }
     }
 }
